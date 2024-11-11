827856c021b8c35108f0166ede10b04e2c11e4b8
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-11 13:13:32 +0000

    cambios en variables de entorno

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_KEY: string
+    readonly NEXT_PUBLIC_SUPABASE_URL: string
+    readonly SUPABASE_ANON_KEY: string
   }
   
   interface ImportMeta {
