d38672063c33327ee46311ab9b9b51ee21a8ffcc
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-19 11:11:19 +0000

    test6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,15 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
 });
